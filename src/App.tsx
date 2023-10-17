f0b2d7f683e6f3053f275af800b8345185cba10c
Joe Grinstead <npm@mailforce.net>
2023-10-17 11:33:25 -0400

    Further on the tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -40,6 +40,16 @@ function renderAppCanvas(props: GPUCanvasDetails) {
     ],
   });
 
+  const GRID_SIZE = 32;
+  const uniformArray = new Float32Array([GRID_SIZE, GRID_SIZE]);
+  const uniformBuffer = device.createBuffer({
+    label: "Grid Uniforms",
+    size: uniformArray.byteLength,
+    usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+  });
+
+  device.queue.writeBuffer(uniformBuffer, 0, uniformArray);
+
   // prettier-ignore
   const vertices = new Float32Array([
     -0.8, -0.8, // Triangle 1 (Blue)
@@ -73,9 +83,14 @@ function renderAppCanvas(props: GPUCanvasDetails) {
   const shader = device.createShaderModule({
     label: "Cell shader",
     code: `
+@group(0) @binding(0) var<uniform> grid: vec2f;
+
 @vertex
-fn vertexMain(@location(0) pos: vec2f) -> @builtin(position) vec4f {
-  return vec4f(pos, 0, 1);
+fn vertexMain(@location(0) pos: vec2f, @builtin(instance_index) instance: u32) -> @builtin(position) vec4f {
+  let i = f32(instance);
+  let cell = vec2f(i % grid.x, floor(i / grid.x));
+  let cellOffset = cell / grid * 2;
+  return vec4f((pos + 1) / grid - 1 + cellOffset, 0, 1);
 }
 
 @fragment
@@ -104,9 +119,21 @@ fn fragmentMain() -> @location(0) vec4f {
     },
   });
 
+  const bindGroup = device.createBindGroup({
+    label: "Cell renderer bind group",
+    layout: cellPipeline.getBindGroupLayout(0),
+    entries: [
+      {
+        binding: 0,
+        resource: { buffer: uniformBuffer },
+      },
+    ],
+  });
+
   pass.setPipeline(cellPipeline);
   pass.setVertexBuffer(0, vertexBuffer);
-  pass.draw(vertices.length / 2);
+  pass.setBindGroup(0, bindGroup);
+  pass.draw(vertices.length / 2, GRID_SIZE * GRID_SIZE);
 
   pass.end();
 
