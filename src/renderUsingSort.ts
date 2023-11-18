e935ec86121fca73709efb07bbf32ea86d941b2d
Joe Grinstead <npm@mailforce.net>
2023-11-18 13:14:47 -0500

    Add projection phase

--- src/renderUsingSort.ts
+++ src/renderUsingSort.ts
@@ -22,7 +22,7 @@ export function renderUsingSort(props: GPUCanvasDetails, splatData: GPUBuffer) {
   });
 
   // This shader renders the guassian splats to a texture
-  const guassianShader = device.createShaderModule({
+  const gaussianShader = device.createShaderModule({
     label: "Gaussian Splatting Shader",
     code: `
 
@@ -45,9 +45,11 @@ struct ProjectedGaussian {
 @group(0) @binding(0) var outputTexture: texture_storage_2d<rgba8unorm, write>;
 @group(1) @binding(0) var<storage> gaussians: array<GaussianSplat>;
 @group(1) @binding(1) var<uniform> camera: mat4x4f; 
-@group(1) @binding(2) var<storage> screenSpaceGaussians: array<ProjectedGaussian>;
+@group(1) @binding(2) var<storage, read_write> projectedGaussians: array<ProjectedGaussian>;
+@group(2) @binding(0) var<storage> renderables: array<ProjectedGaussian>;
 
 override chunkSize: u32 = 16;
+override projectionChunkSize: u32 = 16;
 
 const HARMONIC_COEFF0: f32 = 0.28209479177387814;
 
@@ -67,73 +69,91 @@ fn normalize_opacity(in: f32) -> f32 {
   }
 }
 
+@compute @workgroup_size(projectionChunkSize)
+fn projectGaussians(
+  @builtin(global_invocation_id) index: vec3u,
+) {
+  if (index.x > arrayLength(&gaussians)) {
+    return;
+  }
+
+  let in = gaussians[index.x];
+
+  let camera_space_origin = camera * vec4<f32>(in.origin, 1.0);
+  let z = camera_space_origin.z;
+
+  // quarternion to matrix formula taken from
+  // https://automaticaddison.com/how-to-convert-a-quaternion-to-a-rotation-matrix/
+  let q0 = in.quarternion[0];
+  let q1 = in.quarternion[1];
+  let q2 = in.quarternion[2];
+  let q3 = in.quarternion[3];
+
+  // R (rotation) and S (scales) matrices from Gaussian Splat Paper
+  // technically these are the transposed versions because the gpu is col-major order
+  let R = mat4x4<f32>(
+    2*(q0*q0 + q1*q1) - 1, 2*(q1*q2 - q0*q3), 2*(q1*q3 + q0*q2), 0,
+    2*(q1*q2 + q0*q3), 2*(q0*q0 + q2*q2) - 1, 2*(q2*q3 - q0*q1), 0,
+    2*(q1*q3 - q0*q2), 2*(q2*q3 + q0*q1), 2*(q0*q0 + q3*q3) - 1, 0,
+    0, 0, 0, 1
+  );
+  let SR_T = mat4x4<f32>(
+    exp(in.scales[0]), 0, 0, 0,
+    0, exp(in.scales[1]), 0, 0,
+    0, 0, exp(in.scales[2]), 0,
+    0, 0, 0, 1,
+  ) * R;
+
+  // Σ is from Gaussian Splat paper (section 4, eq. 6)
+  let Σ = transpose(SR_T) * SR_T;
+
+  let JW = mat4x4<f32>(
+    1 / z, 0, 0, 0,
+    0, 1 / z, 0, 0,
+    -1 / z / z, -1 / z / z, 0, 0,
+    0, 0, 0, 0,
+  ) * camera; 
+
+  // x in camera space -> x coordinate in screen space
+  // x as is for now, but z^-1 so derivative is -z^-2
+
+  let Σ_prime_full = JW * Σ * transpose(JW);
+  let Σ_prime_inv = invert_2x2(
+    mat2x2<f32>(
+      Σ_prime_full[0][0], Σ_prime_full[0][1],
+      Σ_prime_full[1][0], Σ_prime_full[1][1],
+    )
+  );
+
+  projectedGaussians[index.x] = ProjectedGaussian(
+    vec3f(camera_space_origin.xy / z, z),
+    vec3f(Σ_prime_inv[0][0], Σ_prime_inv[0][1], Σ_prime_inv[1][1]),
+    vec4<f32>(
+      in.color_sh0 * HARMONIC_COEFF0 + .5,
+      normalize_opacity(in.opacity),
+    ),
+  );
+}
+
 @compute @workgroup_size(chunkSize, chunkSize)
 fn renderGaussians(
   @builtin(workgroup_id) chunkPosition: vec3u,
   @builtin(local_invocation_id) offset: vec3u,
   @builtin(global_invocation_id) pixel: vec3u,
 ) {
-  _ = screenSpaceGaussians[0];
+  let numGaussians = arrayLength(&renderables);
   let coords = 2 * vec2f(pixel.xy) / vec2f(textureDimensions(outputTexture)) - 1;
-
   var color = vec4f(.1, .1, .1, 0);
 
-  let numGaussians = 100; //arrayLength(gaussians);
-  for (var i = 0; i < numGaussians; i++) {
-    let in = gaussians[i];
+  for (var i: u32 = 0; i < numGaussians; i++) {
+    let in = renderables[i];
+    let origin = in.origin;
 
-    let camera_space_origin = camera * vec4<f32>(in.origin, 1.0);
-    let z = camera_space_origin.z;
+    let centered = coords - origin.xy;
 
-    // quarternion to matrix formula taken from
-    // https://automaticaddison.com/how-to-convert-a-quaternion-to-a-rotation-matrix/
-    let q0 = in.quarternion[0];
-    let q1 = in.quarternion[1];
-    let q2 = in.quarternion[2];
-    let q3 = in.quarternion[3];
-
-    // R (rotation) and S (scales) matrices from Gaussian Splat Paper
-    // technically these are the transposed versions because the gpu is col-major order
-    let R = mat4x4<f32>(
-      2*(q0*q0 + q1*q1) - 1, 2*(q1*q2 - q0*q3), 2*(q1*q3 + q0*q2), 0,
-      2*(q1*q2 + q0*q3), 2*(q0*q0 + q2*q2) - 1, 2*(q2*q3 - q0*q1), 0,
-      2*(q1*q3 - q0*q2), 2*(q2*q3 + q0*q1), 2*(q0*q0 + q3*q3) - 1, 0,
-      0, 0, 0, 1
-    );
-    let SR_T = mat4x4<f32>(
-      exp(in.scales[0]), 0, 0, 0,
-      0, exp(in.scales[1]), 0, 0,
-      0, 0, exp(in.scales[2]), 0,
-      0, 0, 0, 1,
-    ) * R;
-
-    // Σ is from Gaussian Splat paper (section 4, eq. 6)
-    let Σ = transpose(SR_T) * SR_T;
-
-    let JW = mat4x4<f32>(
-      1 / z, 0, 0, 0,
-      0, 1 / z, 0, 0,
-      -1 / z / z, -1 / z / z, 0, 0,
-      0, 0, 0, 0,
-    ) * camera; 
-
-    // x in camera space -> x coordinate in screen space
-    // x as is for now, but z^-1 so derivative is -z^-2
-
-    var Σ_prime_full = JW * Σ * transpose(JW);
-
-    let Σ_prime_inv = invert_2x2(
-      mat2x2<f32>(
-        Σ_prime_full[0][0], Σ_prime_full[0][1],
-        Σ_prime_full[1][0], Σ_prime_full[1][1],
-      )
-    );
-
-    let centered = coords - camera_space_origin.xy / z;
-
-    if (length(centered) < .1) {
+    if (length(centered) < .01) {
       let splatColor = vec4<f32>(
-        in.color_sh0 * HARMONIC_COEFF0 + .5,
+        in.color.xyz,
         1,
       ); 
       color = splatColor;
@@ -145,11 +165,7 @@ fn renderGaussians(
     //   let splatColor = vec4<f32>(
     //     in.color_sh0 * HARMONIC_COEFF0 + .5,
     //     1,
-    //   ); 
-
-    //   if (alpha > .4 && z < prevZ) {
-    //     prevZ = z;
-    //   }
+    //   );
 
     //   color = (1 - alpha) * color + alpha * splatColor;
     //   // color = vec4f(0, alpha, 0, 1);
@@ -161,10 +177,18 @@ fn renderGaussians(
 `,
   });
 
+  const projectGaussiansPipeline = device.createComputePipeline({
+    layout: "auto",
+    compute: {
+      module: gaussianShader,
+      entryPoint: "projectGaussians",
+    },
+  });
+
   const guassianPipeline = device.createComputePipeline({
     layout: "auto",
     compute: {
-      module: guassianShader,
+      module: gaussianShader,
       entryPoint: "renderGaussians",
     },
   });
@@ -181,7 +205,8 @@ fn renderGaussians(
 
   let projectedGaussianBufferSize = 0;
   let projectedGaussianBuffer: undefined | GPUBuffer;
-  let guassianDataBindGroup: undefined | GPUBindGroup;
+  let projectedDataBindGroups: undefined | Array<GPUBindGroup>;
+  let renderDataBindGroups: undefined | Array<GPUBindGroup>;
 
   // creates a shader that needs to draw 4 points, one for each corner of the screen
   const outputShader = device.createShaderModule({
@@ -279,21 +304,50 @@ fn fragment_main(@location(0) fragUV: vec2f) -> @location(0) vec4f {
         usage: GPUBufferUsage.STORAGE,
       });
 
-      guassianDataBindGroup = device.createBindGroup({
-        label: "Specific Gaussian Data to Render",
-        layout: guassianPipeline.getBindGroupLayout(1),
-        entries: [
-          { binding: 0, resource: { buffer: splatData } },
-          { binding: 1, resource: { buffer: cameraBuffer } },
-          { binding: 2, resource: { buffer: projectedGaussianBuffer } },
-        ],
-      });
+      projectedDataBindGroups = [
+        device.createBindGroup({
+          layout: projectGaussiansPipeline.getBindGroupLayout(0),
+          entries: [],
+        }),
+        device.createBindGroup({
+          label: "Specific Gaussian Data to Render",
+          layout: projectGaussiansPipeline.getBindGroupLayout(1),
+          entries: [
+            { binding: 0, resource: { buffer: splatData } },
+            { binding: 1, resource: { buffer: cameraBuffer } },
+            { binding: 2, resource: { buffer: projectedGaussianBuffer } },
+          ],
+        }),
+      ];
+
+      renderDataBindGroups = [
+        guassianBindGroup,
+        device.createBindGroup({
+          layout: guassianPipeline.getBindGroupLayout(1),
+          entries: [],
+        }),
+        device.createBindGroup({
+          layout: guassianPipeline.getBindGroupLayout(2),
+          entries: [
+            { binding: 0, resource: { buffer: projectedGaussianBuffer } },
+          ],
+        }),
+      ];
     }
+
+    const projectionPass = encoder.beginComputePass();
+    projectionPass.setPipeline(projectGaussiansPipeline);
+    projectedDataBindGroups!.forEach((group, i) => {
+      projectionPass.setBindGroup(i, group);
+    });
+    projectionPass.dispatchWorkgroups(Math.ceil(numSplats / CHUNK_SIZE));
+    projectionPass.end();
 
     const guassianPass = encoder.beginComputePass();
     guassianPass.setPipeline(guassianPipeline);
-    guassianPass.setBindGroup(0, guassianBindGroup);
-    guassianPass.setBindGroup(1, guassianDataBindGroup!);
+    renderDataBindGroups!.forEach((group, i) => {
+      guassianPass.setBindGroup(i, group);
+    });
     guassianPass.dispatchWorkgroups(chunkDims.x, chunkDims.y);
     guassianPass.end();
 
